006b91db53cd219edcdd15c888c589bfe03e469a
nathanbodie <nathan@bodie.dev>
2026-03-08 16:29:35 -0400

    update hyprland config for dual monitor setup, switch to ghostty and wofi

--- .config/nvim/lua/nate/plugins/init.lua
+++ .config/nvim/lua/nate/plugins/init.lua
@@ -61,24 +61,9 @@ return {
 		},
 		opts_extend = { "sources.default" },
 	},
-	{
-		"lopi-py/luau-lsp.nvim",
-		opts = {
-			platform = {
-				type = "roblox",
-			},
-			types = {
-				roblox_security_level = "PluginSecurity",
-			},
-			sourcemap = {
-				enabled = true,
-				autogenerate = true,
-				rojo_project_file = "default.project.json",
-				sourcemap_file = "sourcemap.json",
-			},
-		},
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-	},
+  {
+    'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    build = ':TSUpdate'
+  },
 }
