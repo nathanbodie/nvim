6d0888e149aa3d446e6e5ba13f149a31e3e83d65
nathanbodie <nathan@bodie.dev>
2026-09-13 22:45:26 -0400

    go to definition 'gd', '<leader>ld' to show diagnostic, '<leader>ly' to yank diagnostic

--- init.lua
+++ init.lua
@@ -21,10 +21,6 @@ local map = vim.keymap.set
 -- system clipboard
 map({ 'n', 'v' }, '<leader>y', '"+y')
 map({ 'n', 'v' }, '<leader>d', '"+d')
-
-map('n', '<leader>lf', function()
-  require('conform').format({ lsp_format = 'fallback' })
-end)
 map('n', '<leader>f', '<Cmd>Pick files<CR>')
 map('n', '<leader>r', '<Cmd>Pick buffers<CR>')
 map('n', '<leader>/', '<Cmd>Pick grep_live<CR>')
@@ -37,7 +33,27 @@ end)
 map('n', '<leader>mh', '<Cmd>Pick help<CR>')
 map('n', '<leader>mk', '<Cmd>Pick keymaps<CR>')
 map('n', '<leader>o', '<Cmd>Oil<CR>')
-map('n', '<leader>e', '<Cmd>lua vim.diagnostic.open_float()<CR>')
+map('n', '<leader>lf', function()
+  require('conform').format({ lsp_format = 'fallback' })
+end)
+map('n', '<leader>ld', '<Cmd>lua vim.diagnostic.open_float()<CR>') -- <C-w>d also does this, but this is just for symmetry with <leader>ly
+map('n', '<leader>ly', function()
+  local line = vim.api.nvim_win_get_cursor(0)[1] - 1
+  local diagnostics = vim.diagnostic.get(0, { lnum = line })
+
+  if #diagnostics == 0 then
+    vim.notify('No diagnostics on this line')
+    return
+  end
+
+  local messages = vim.tbl_map(function(d)
+    return d.message
+  end, diagnostics)
+
+  vim.fn.setreg('+', table.concat(messages, '\n'))
+  vim.notify('Diagnostic copied to clipboard')
+end)
+map('n', 'gd', vim.lsp.buf.definition)
 
 -- markdown preview
 map('n', '<leader>mp', '<Cmd>Markview splitToggle<CR>')
